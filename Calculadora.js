bdeff944938d403ba6ce52dd6285c83bf1d39161
C4fesin <hurikcamargo@gmail.com>
2024-05-29 12:39:19 -0300

    Desafio

---
+++ Calculadora.js
@@ -0,0 +1,23 @@
+ commit
+ let vitorias = 95;
+let derrotas = 20; 
+let quantidadeDeVitorias = (vitorias - derrotas);
+let saldoDeVitorias = quantidadeDeVitorias;
+let nivel 
+if (quantidadeDeVitorias <= 10  ){
+    nivel = "Ferro";
+}else  if (quantidadeDeVitorias >= 11 && quantidadeDeVitorias <= 20) {
+    nivel = "Bronze";
+} else if (quantidadeDeVitorias >= 21 && quantidadeDeVitorias <= 50) {
+    nivel = "Prata";
+}  else if (quantidadeDeVitorias >= 51 && quantidadeDeVitorias <= 80) {
+    nivel = "Ouro";
+}else if (quantidadeDeVitorias >= 81 && quantidadeDeVitorias <= 90) {
+     nivel ="Diamante";
+}else if (quantidadeDeVitorias >= 91 && quantidadeDeVitorias <= 100) {
+    nivel = "Lendário";
+}else if (quantidadeDeVitorias <= 101 ) {
+    nivel = "Imortal";
+}
+
+console.log("O Herói tem de saldo de " + saldoDeVitorias + " está no nível de " + nivel)
